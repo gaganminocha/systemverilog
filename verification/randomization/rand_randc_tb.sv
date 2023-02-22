c1b0cbe243814e08d35115eef885681d0cbcce67
gaganminocha <gagan.minocha@outlook.com>
2023-02-22 23:33:57 +0530

    adding rand randc randomize

---
+++ verification/randomization/rand_randc_tb.sv
@@ -0,0 +1,19 @@
+module rand_randc_tb();
+	
+	class rand_packet;
+		rand  bit rnd ;
+		randc bit rndc;
+	endclass
+
+	rand_packet pack_obj ;
+	//pack_obj = new();
+
+	initial
+	begin
+		pack_obj = new();
+		$monitor("rand = %d \t randc = %d \n", pack_obj.rnd, pack_obj.rndc);
+		repeat(10) #5 pack_obj.randomize();
+		$finish;
+	end
+
+endmodule
