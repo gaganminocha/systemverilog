66ad7cf8a62f4e3879c2fa2804b17e0e84fa2186
gaganminocha <gagan.minocha@outlook.com>
2023-07-04 19:43:53 -0400

    Constraint if-else

---
+++ verification/randomization_and_constraints/constraint_if_else/cifel.sv
@@ -0,0 +1,38 @@
+`timescale 1ns/100ps
+
+class cimp;
+    typedef enum {xs, s, m, l, xl} size;
+   
+    rand bit [4:0] pkt;
+    rand size pkt_size;
+
+    constraint ps
+    {
+        if(pkt_size == xs)
+           pkt inside{[ 0: 8]};
+        else if(pkt_size == s)
+            pkt inside{[ 9:14]};
+        else if (pkt_size ==  m)
+            pkt inside{[15:22]};
+        else if (pkt_size ==  l)
+            pkt inside{[23:27]};
+        else
+            pkt inside{[28:31]};
+    }
+
+    function void post_randomize();
+        $display("pkt_size= %d\t pkt:%d", pkt_size.name, pkt);
+    endfunction
+
+endclass
+
+
+module tb;
+    cimp obj;
+
+    initial
+    begin
+        obj = new();
+        repeat (20) #5 obj.randomize();
+    end
+endmodule
---
+++ verification/randomization_and_constraints/constraint_if_else/readme.txt
@@ -0,0 +1,35 @@
+Constraint If-else
+    if (Expression)
+        constraint
+    else if
+        constraint
+    else
+        constraint
+
+
+--------------------
+Problem:
+--------------------
+
+enum size having:
+    xs, s, m, l and xl
+
+pkt     - 5 bits
+
+constraint:
+    for xs: pkt is  0 to  8
+    for  s: pkt is  9 to 14
+    for  m: pkt is 15 to 22
+    for  l: pkt is 23 to 27
+    for xl: pkt is 28 to 31
+
+
+randomize 20 times
+
+
+--------------------
+Takeaways:
+--------------------
+-similar behavior can be achieved using implication (->) in constraint.
+-pkt_size is of enum type 'size', so for printing it, use pkt_size.name
+
