96039c1950a0ce5413b3d469143c8b46b65c7c59
gaganminocha <gagan.minocha@outlook.com>
2023-07-06 13:46:52 -0400

    Constraint using Function

---
+++ verification/randomization_and_constraints/constraint_function/cf.sv
@@ -0,0 +1,39 @@
+`timescale 1ns/100ps
+
+class cf;
+
+    rand byte pkt[];
+    rand bit [3:0] addr;
+
+    function bit [4:0] size_maker(bit [3:0] addr);
+        size_maker = addr + 10;  
+    endfunction
+
+    constraint pkt_s
+    {
+        pkt.size == size_maker(addr);
+    }
+
+    function void post_randomize();
+        $display("time: %d, addr: %d, pkt_size: %d", $time, addr, pkt.size);
+    endfunction
+
+endclass
+
+
+module tb;
+    cf obj;
+
+    initial
+    begin
+        obj = new();
+        repeat (10)
+        begin
+            #5 obj.randomize();
+            $display("sm: %d", obj.size_maker(obj.addr));
+        end
+    end
+
+endmodule
+
+
---
+++ verification/randomization_and_constraints/constraint_function/readme.txt
@@ -0,0 +1,30 @@
+Constraint Function
+
+
+--------------------
+Problem:
+--------------------
+
+pkt     - byte type dynamic array
+addr    - rand 4-bit
+
+function:
+    to return addr + 10.
+
+constraint pkt_s:
+    pkt  array size     < return val of function
+
+
+randomize 10 times
+
+
+--------------------
+Takeaways:
+--------------------
+-foreach loop syntax
+    foreach ( variable[iterator] )  <task to be done>
+
+-for printing array: use $display inside foreach. 
+
+
+
