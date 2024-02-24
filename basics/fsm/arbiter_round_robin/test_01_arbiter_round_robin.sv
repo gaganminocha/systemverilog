5d37165bdefbd34cb844365cd109734683c8131d
gaganminocha <gagan.minocha@outlook.com>
2024-02-24 00:55:19 -0500

    test 01 for arbiter

--- basics/fsm/arbiter_round_robin/test_01_arbiter_round_robin.sv
+++ basics/fsm/arbiter_round_robin/test_01_arbiter_round_robin.sv
@@ -5,7 +5,15 @@ module tb_arbiter_round_robin();
     reg clk, rst;
     reg [3:0] req;
     wire [3:0] gnt;
+    
     arbiter_round_robin inst_arbiter_round_robin(clk, rst, req, gnt);
+    
+    initial
+    begin
+        clk = 1;
+        forever #5 clk = ~clk;
+    end
+
 
 
 endmodule
