94c12b0de04d0b6582e2e4eabe0d36855e58026a
gaganminocha <gagan.minocha@outlook.com>
2024-03-26 18:35:46 -0400

    fork join_none

---
+++ verification/sv_basics_oops/fork_join_none/fjn.sv
@@ -0,0 +1,29 @@
+// SV - fork join none
+//
+// Author: Gagan Minocha (igaganminocha@gmail.com)
+//
+
+
+
+module tb;
+    
+    initial
+    begin
+        fork
+            begin : proc_1
+                $display($realtime, " Proc 1 start");
+                #5
+                $display($realtime, " Proc 1 end");
+            end
+            
+            begin : proc_2
+                $display($realtime, " Proc 2 start");
+                #20
+                $display($realtime, " Proc 2 end");
+            end
+        join_none
+        $display($realtime, " Join None");
+        
+    end
+
+endmodule
